cedd07b04b3baf7627b37d81af5e56de64785acc
lucas-larsson <lucas.a.larsson@gmail.com>
2023-04-17 23:30:16 +0200

    refactored

--- backend/middlewares/auth/authDAO.js
+++ backend/middlewares/auth/authDAO.js
@@ -1,17 +1,17 @@
 const { sendQuery } = require('../../utils/dbIntegration/dbConfig'),
   { roleMap } = require('../../utils/maps');
 
-const createUser = async (applicant) => {
+const createUser = async (user) => {
   const query = `INSERT INTO person (name, surname, pnr, email, username, password, role_id) 
     VALUES ($1, $2, $3, $4, $5, crypt($6, 'password'), $7) RETURNING name, surname, pnr, email, username, role_id, person_id`;
   try {
     const result = await sendQuery(query, [
-      applicant.name,
-      applicant.surname,
-      applicant.pnr,
-      applicant.email,
-      applicant.username,
-      applicant.password,
+      user.name,
+      user.surname,
+      user.pnr,
+      user.email,
+      user.username,
+      user.password,
       roleMap.applicant,
     ]);
     return result.rows[0];
