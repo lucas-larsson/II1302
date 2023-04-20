bc9aa80813abafa0ba05cfe6290fc65f8bae1e3b
lucas-larsson <lucas.a.larsson@gmail.com>
2023-04-21 01:24:20 +0200

    Revert "test deployment"
    This reverts commit 604a1325add8ebf89d1e4fa7b9839d65aeeab74d.

--- backend/api-routes/errorcodes.js
+++ backend/api-routes/errorcodes.js
@@ -1,20 +1,20 @@
 const { sendErrorCodes } = require('../middlewares/errorcodes');
 module.exports = {
-  get: [sendErrorCodes],
+	get: [sendErrorCodes],
 };
 
 module.exports.get.apiDoc = {
-  tags: ['error-codes'],
-  responses: {
-    200: {
-      description: 'Successfully fetched all error codes',
-      content: {
-        'application/json': {
-          schema: {
-            $ref: '#/components/schemas/ListOfErrorCodes',
-          },
-        },
-      },
-    },
-  },
+	tags: ['error-codes'],
+	responses: {
+		200: {
+			description: 'Successfully fetched all error codes',
+			content: {
+				'application/json': {
+					schema: {
+						$ref: '#/components/schemas/ListOfErrorCodes',
+					},
+				},
+			},
+		},
+	},
 };
