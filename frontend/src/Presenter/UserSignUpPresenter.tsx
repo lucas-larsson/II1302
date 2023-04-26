6ca6bc83dc820a335fe30effac8f0485d50701fc
Deni Persson <deni.persson@gmail.com>
2023-04-26 14:50:20 +0200

    Functional SignUp Component
    -The signup component is now connected to the backend.
    - Basic field formatting implemented using new helper file (Formatting.js)
    
    Next to fix is login and then creating a context to stay logged in.

--- frontend/src/Presenter/UserSignUpPresenter.tsx
+++ frontend/src/Presenter/UserSignUpPresenter.tsx
@@ -1,5 +1,76 @@
+import React from "react";
 import UserSignUpView from "../View/UserSignUpView";
+import { useNavigate } from "react-router-dom";
+
+import { containsNumber, containsSymbol, isValidEmail } from "../Helpers/Formatting";
 
 export default function UserSignUpPresenter() {
-  return <UserSignUpView></UserSignUpView>;
+  const [errorMsg, setErrorMsg] = React.useState<string>("");
+  const navigate = useNavigate();
+
+  async function signUp(firstName: string, lastName: string, email: string, password: string) {
+
+    if(firstName.length < 2 || lastName.length < 2){
+      setErrorMsg("Your names must be at least 2 letters long.");
+      console.log(errorMsg);
+      return;
+    }
+    if(containsNumber(firstName) || containsSymbol(firstName) || containsNumber(lastName) || containsSymbol(lastName)){
+      setErrorMsg("Your name(s) contain illegal symbols.");
+      console.log(errorMsg);
+      return;
+    }
+    if(!isValidEmail(email)){
+      setErrorMsg("Your email is in an invalid format.");
+      console.log(errorMsg);
+      return;
+    }
+    if(password.length < 6){
+      setErrorMsg("Your password must be at least 6 characters long");
+      console.log(errorMsg);
+      return;
+    }
+
+    const response = await fetch(
+      "https://ii1302-backend-wdsryxs5fa-lz.a.run.app/api/signup/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: firstName,
+          surname: lastName,
+          email: email,
+          password: password,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      const user = data.user;
+      const session = data.session;
+      console.log("New user:", user);
+      console.log("Session:", session);
+      setErrorMsg(""); // Clear any previous error message
+
+      // Navigate to the home page
+      navigate("/");
+    } else {
+      let errorText = "";
+
+      switch (response.status) {
+        case 400:
+          errorText = "There is already a user with this email address. Please try another email or log in.";
+          break;
+        default:
+          errorText = `An error occurred: ${response.statusText}`;
+      }
+      console.log(errorText);
+      setErrorMsg(errorText);
+    }
+  }
+
+  return <UserSignUpView signUp={signUp} errorMsg={errorMsg}></UserSignUpView>;
 }
--- frontend/src/View/UserSignUpView.tsx
+++ frontend/src/View/UserSignUpView.tsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Text, OuterBox, Button, Input } from "../Styles/BaseStyles";
+import { Text, OuterBox, Button, Input, ErrorText} from "../Styles/BaseStyles";
 
-interface Props{
-
+interface Props {
+    signUp:(firstName:string, lastName:string, email:string, password:string)=>void;
+    errorMsg:string;
 }
 
-export default function UserSignUpView(props:Props){
+export default function UserSignUpView(props: Props) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showErrorBox, setShowErrorBox] = useState<Boolean>(false);
 
-    function signUpAttempt(){
-
+  useEffect(() => {
+    if (props.errorMsg !== "") {
+      setShowErrorBox(true);
+    } else {
+      setShowErrorBox(false);
     }
-    
-    return ( 
+  }, [props.errorMsg]);
+  
+
+  function signUpAttempt() {
+    props.signUp(firstName, lastName, email, password);
+  }
+
+  return (
     <OuterBox>
-        <Text><b>Register now to start using the app!</b></Text>
-        <Input type="firstName" placeholder="First name" required />
-        <Input type="lastName" placeholder="Last name" required />
-        <Input type="email" placeholder="Email" required />
-        <Input type="password" placeholder="Password" required />
-        <Text></Text> 
-        <Button onClick={signUpAttempt} type="submit">Sign Up</Button>
-        <Text></Text>
-    </OuterBox>)
+      <Text>
+        <b>Register now to start using the app!</b>
+      </Text>
+    
+      { showErrorBox ? 
+        <OuterBox><ErrorText>{props.errorMsg}</ErrorText> </OuterBox>
+        :
+        null
+      }
+
+      <Input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="First name"
+        required
+      />
+      <Input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        placeholder="Last name"
+        required
+      />
+      <Input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <Text></Text>
+      <Button onClick={signUpAttempt} type="button">
+        Sign Up
+      </Button>
+      <Text></Text>
+    </OuterBox>
+  );
 }
