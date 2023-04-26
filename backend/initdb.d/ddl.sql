0bce48343fbca268fba826656d08551dcaba61e1
lucas-larsson <lucas.a.larsson@gmail.com>
2023-04-27 00:46:31 +0200

    added "plant_data" table

--- backend/initdb.d/ddl.sql
+++ backend/initdb.d/ddl.sql
@@ -1,4 +1,4 @@
-create table public.role
+create table if not exists public.role
 (
     role_id integer generated always as identity
         primary key,
@@ -8,7 +8,7 @@ create table public.role
 alter table public.role
     owner to postgres;
 
-create table public.person
+create table if not exists public.person
 (
     person_id integer generated always as identity
         primary key,
@@ -25,7 +25,7 @@ create table public.person
 alter table public.person
     owner to postgres;
 
-create table public.sessions
+create table if not exists public.sessions
 (
     session_id      uuid default gen_random_uuid() not null
         constraint sessions_pk
@@ -38,17 +38,31 @@ create table public.sessions
 alter table public.sessions
     owner to postgres;
 
-create table public.iot_auth
+create table if not exists public.iot_auth
 (
-    id            integer not null
+    id              integer not null
         constraint iot_auth_pk
             primary key
         constraint iot_auth_person_person_id_fk
             references public.person,
-    device_id     varchar not null,
+    device_id       varchar not null,
     device_password varchar
 );
 
 alter table public.iot_auth
     owner to postgres;
+
+create table if not exists public.plant_data
+(
+    device_id        varchar,
+    last_watered     timestamp,
+    moisture_level double precision,
+    user_id          integer
+        constraint plant_data_person_person_id_fk
+            references public.person
+);
+
+alter table public.plant_data
+    owner to postgres;
+
 
