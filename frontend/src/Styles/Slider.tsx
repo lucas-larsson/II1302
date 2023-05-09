2844bdf80afab0fbf2ec23095320ba62d7a7fe2d
Donglin Liu <73525819+AkemiHomura666@users.noreply.github.com>
2023-05-09 11:59:01 +0200

    Update Slider.tsx

--- frontend/src/Styles/Slider.tsx
+++ frontend/src/Styles/Slider.tsx
@@ -11,14 +11,47 @@ const SliderStyled = styled.input`
   outline: none;
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
+          position: relative;
     appearance: none;
     width: 25px;
     height: 25px;
     border-radius: 100px;
     background-color: #ffffffce;
     cursor: pointer;
+    z-index: 1001;
     box-shadow: -1010px 0 0 1000px #2a9d8f; // Increase the shadow size
   }
+  &::before {
+      content: '';
+      position: absolute;
+      top: 298px;
+      left: 25%;
+      width: 15%;
+      height: 25px;
+      border-radius: 100px;
+      border-top-right-radius: 0%;
+      border-bottom-right-radius: 0%;
+      border-right: 5px solid #fff;
+      opacity: 0.1;
+      background-color: #000;
+      z-index: 1000;
+    }
+  &::after {
+      content: '';
+      position: absolute;
+      top: 298px;
+      right: 25%;
+      width: 35%;
+      height: 25px;
+      border-radius: 100px;
+      border-bottom-left-radius: 0%;
+      border-top-left-radius: 0%;
+      border-left: 5px solid #fff;
+      opacity: 0.3;
+      background-color: #98e098c7;
+      z-index: 1002;
+    }
+    
 `;
 
 const SliderValue = styled.span`
